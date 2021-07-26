9e0847b7311317c18fe8265d3eb4f1a3666fdd4c
ahza64 <justinmichaelpettit@gmail.com>
2021-07-26 11:43:36 -0700

    update external links to internal

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -13,7 +13,7 @@ return (
     </div>
     <div className="container1">
       <div className="box1">
-      <a href="/professional" style={{color: "white", textDecoration: "none"}}>
+      <Link to="/professional" style={{color: "white", textDecoration: "none"}}>
         <span></span>
         <div id="slot1" className="content1">
         <img id="slot1img" src={oldJustin} alt="slot1 image"/>
@@ -21,23 +21,23 @@ return (
         With a background in HTML, CSS, Javascript, Node.js, MongoDB aggregation, and React/React Hooks, this multifaceted man will most likely add functionality you didn't expect. Full-stack is a balance between my passion for algorithm development on the back end and creating intuitive and aesthetically pleasing interaction on the front.
         I come from a family of creative types and have a lot of experience in the design and production of actual products in use. A family that always has projects going. Design comes like a second language. End-to-end design speciality with a new found focus on front end.
         </div>
-      </a>
+      </Link>
       </div>
     </div>
     <div className="container2">
       <div className="box2">
-      <a href="/about" style={{color: "white", textDecoration: "none"}}>
+      <Link to="/about" style={{color: "white", textDecoration: "none"}}>
         <span></span>
         <div id="slot2" className="content2">
         <img id="slot2img" src={youngJustin} alt="slot2 image"/>
           To quickly encapsulate the things I enjoyed and grew from in my childhood that impacts my adulthood and professionalism today. (Not to be too corporate, but…) let's take a look at easily relatable begginnings that lead to greater things; Star Wars, Ninja Turtles, and Legos. Being the philosopher I seem to have always been, older Star Wars lore had a great appeal to me and may have been the strongest influence on me to seeking truth and harmony in the nature of things and the beginnings of buddhism and later Western Philosophy to help me navigate the complexities of self and direction within the real tangible world we exsist in. Ninja Turtles was the beginning of what it means to me, to be a team player and sometimes challenge the brotherhood bond against ethics, morals and honor. Legos, on the other hand, had the greatest impact on my tangible professional career. I’ve always liked taking things apart and putting them back together. A computer’s hardware falls into these tangible things, but what a computer creates, the software, are just imaginary (digital) “things” you take apart and put back together, and ever since I wired up my first relay, I’ve been thoroughly enjoying it. It just took me a while to get over my “tough guy” mindset about needing to use my body to earn a living.
         </div>
-      </a>
+      </Link>
       </div>
     </div>
     <div className="container3">
       <div className="box3">
-      <a href="/contact">
+      <Link to="/contact">
 
         <span></span>
         <div id="slot3" className="content3">
@@ -46,7 +46,7 @@ return (
         <img id="slot2img" src={linkedinLogo} alt="slot2 image"/>
         </div>
 
-      </a>
+      </Link>
       </div>
     </div>
     {/*
